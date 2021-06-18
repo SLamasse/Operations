98b115203600f9bbd120c6d554a04f49edf12a82
SLamasse <74814681+SLamasse@users.noreply.github.com>
2021-06-18 15:40:41 +0200

    Add files via upload

--- fractions.py
+++ fractions.py
@@ -18,7 +18,7 @@ def abrevie_rout(listrout, sep="/"):
     denom = decomp_fact_premier(b[0]) # dénominateur
 
     # 3. On appel une fonction qui permet d'enlever les facteurs communs aux dénominateurs et numérateurs
-    # puis de les multiplier entre eux 
+    # puis de les multiplier entre eux
     res1 = multiplierlist(SupprimerValeursIdentiques(num,denom))
     res2 = multiplierlist(SupprimerValeursIdentiques(denom,num))
     # 4. on produit une chaine qui permette de l'écrire comme Nicolas Chuquet
@@ -35,7 +35,6 @@ def reduire_rout(listrout, sep="/"):
 
     # 1. On isole dénominateurs et nominateurs de chacune des fractions
     numerateur, denominateur = transformfrac(listrout, sep)
-    multiplie_denominateur =  multiplierlist(denominateur)
 
     # 2. "multiplier les denominateurs particuliers l'ung par l'aultre
     #pour trouver denominateurs commun"
@@ -45,19 +44,33 @@ def reduire_rout(listrout, sep="/"):
     # mais il n'y a pas d'exemple de ce type.
     # Pour Chuquet, en revanche, il est inutile de multiplier tous les termes entre
     # eux. Il faut aupravant savoir si certains des dénominateurs sont
-    # contenus dans d'autres --- > il y a évidmment une économie de
+    # contenus dans d'autres --- > il y a évidemment une économie de
     #calcul
-    rs = soustrairelistemultiples(denominateur)
-    print(rs)
+    if T_GrandDenomDiviseurCommun(denominateur)==True:
+        rs = [max(denominateur)]
+    else:
+        # sinon on multiplie tous les dénominateurs entre eux 
+#        rs = soustrairelistemultiples(denominateur)
+        rs = denominateur
+
     i = 0
     reduction = []
     while i < len(numerateur):
         denom = denominateur[i]
         num = numerateur[i]
-        intermediaire = multiplie_denominateur//denom
-        reduction.append(str(intermediaire * num) + "/" + str(multiplie_denominateur))
+        denominateur_commun = multiplierlist(rs)
+       # multiplicateur = denominateur_commun//denom
+       # faudrait mieux faire une division avec des entiers mais bon 
+        multiplicateur = denominateur_commun//denom
+        d1 = denom*multiplicateur
+#        print(str(denom) + " x " + str(multiplicateur) + " = " + str(d1) )
+        n1 = num*multiplicateur
+        reduction.append(str(n1) + "/" + str(d1))
         i += 1 
     return reduction
+
+
+
 
 def reduc_rout_de_rout(listfrac, sep="/"):
     # apparaît au folio 10 v
@@ -75,17 +88,17 @@ def reduc_rout_de_rout(listfrac, sep="/"):
 #rout = [["12/60"], ["1/24"],["12/68"], ["6/8"], ["1/2","4/7", "10/11"]]
 
 
-rout =  ["1/4","3/2","1/2","1/6", "4/7", "1/5","10/11"]
-#out = ["1/2","1/3", "3/4","5/6", "3/8", "7/12", "19/24"]
-#print(reduire_rout(rout))
-abr = abrevie_rout(rout)
-print(abr)
+#rout =  ["1/4","3/2","1/2","1/6", "4/7", "1/5","10/11"]
+rout = ["1/2","1/3", "3/4","5/6", "3/8", "7/12", "19/24"]
+res = reduire_rout(rout)
+print(res)
+
 #for elt in rout:
 #    if len(elt)>1:
+#        abr = abrevie_rout(elt)
 #        frac = reduc_rout_de_rout(elt)
 #        print(frac)
 #    else:
 #        frac = elt
-#    abr = abrevie_rout(frac)
-#    print(elt)
-#    print(abr)
+#        abr = abrevie_rout(frac)
+#        print(abr)
